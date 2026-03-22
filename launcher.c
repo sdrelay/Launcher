6b7884216b4e394f36908810ce92f22fa605509f
Sadri <108393507+sdrelay@users.noreply.github.com>
2026-03-22 18:52:32 +0800

    Create launcher.c

---
+++ launcher.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // Run your custom script/program
+    system("/custom.sh");
+    
+    // Execute the real TeamSpeak server with all arguments
+    execv("./ts3server_real", argv);
+    
+    return 1;
+}
